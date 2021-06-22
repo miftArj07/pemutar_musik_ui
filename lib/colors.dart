09cf5e18434f397fd2ab6f4564edee5b1173b331
miftArj07 <miftahunnadhir4@gmail.com>
2021-06-22 15:37:35 +0700

    update commit

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+Color primaryColor = Color(0xFFCBDAED);
+Color darkPrimaryColor = Color(0xFF2E346F);
